5c13bf5d3471060537b2a6486d810eaad109d05f
Ruchin Khangar <64952902+RavenTFR@users.noreply.github.com>
2024-10-01 14:41:46 +0530

    Add files via upload

---
+++ task 9.sql
@@ -0,0 +1,12 @@
+alter table Employee_details
+rename to employee_database
+
+select * from employee_database
+
+alter table employee_database 
+rename firstname to name
+
+alter table employee_database 
+rename lastname to surname
+
+select * from employee_database
